e92e4726dd72c4ac01c92373f189445c096bdbb5
Dmitrii Trifonov <comanche.ak47@yandex.ru>
2019-09-19 01:40:38 +0300

    added lines maker

--- src/com/mitya1234/kotlin/csv/utils/Example.kt
+++ src/com/mitya1234/kotlin/csv/utils/Example.kt
@@ -1,5 +1,16 @@
 package com.mitya1234.kotlin.csv.utils
 
+
+import com.mitya1234.kotlin.csv.utils.tools.CSVUtil.Companion.getCSVLines
+
+import java.nio.file.Paths
+
 fun main(args: Array<String>){
         println("Hello")
+        val path = Paths.get("").toAbsolutePath()
+        println(path.toString())
+
+        getCSVLines("./resources/Book.csv")?.forEach { println(it) }
+
+        println("end")
 }
