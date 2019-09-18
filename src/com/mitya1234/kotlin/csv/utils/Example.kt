352049631ac51d336516fd56d70e70dd6fe90432
Dmitrii Trifonov <comanche.ak47@yandex.ru>
2019-09-18 22:30:15 +0300

    Added CSVUtil class

---
+++ src/com/mitya1234/kotlin/csv/utils/Example.kt
@@ -0,0 +1,5 @@
+package com.mitya1234.kotlin.csv.utils
+
+fun main(args: Array<String>){
+        println("Hello")
+}
---
+++ src/com/mitya1234/kotlin/csv/utils/tools/CSVUtil.kt
@@ -0,0 +1,9 @@
+package com.mitya1234.kotlin.csv.utils.tools
+
+class CSVUtil {
+    companion object {
+        fun readFile(pathToFile: String): Array<String> {
+            return emptyArray()
+        }
+    }
+}
