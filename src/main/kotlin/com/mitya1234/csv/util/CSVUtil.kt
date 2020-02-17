758df61752fe0f826452eb14dd77116a8813f07b
Dmitrii Trifonov <53140442+DmitriiTrifonov@users.noreply.github.com>
2020-02-17 11:55:30 +0300

    Add files via upload

--- src/main/kotlin/com/mitya1234/csv/util/CSVUtil.kt
+++ src/main/kotlin/com/mitya1234/csv/util/CSVUtil.kt
@@ -1,9 +1,13 @@
-package com.mitya1234.kotlin.csv.utils.tools
+package com.mitya1234.csv.util
 
 import java.io.File
 
 private fun readFile(pathToFile: String): String? {
     return File(pathToFile).readText()
+}
+
+private fun getFileName(pathToFile: String): String? {
+    return  File(pathToFile).name
 }
 
 private fun removeFEFF(inputStr: String?): String? {
@@ -22,5 +26,22 @@ fun getCsvText(pathToCsv: String): String? {
 
 fun getCSVLines(pathToCsv: String): List<String>? {
     return getCsvText(pathToCsv)?.split("\n")
+}
+
+fun makeDataClass(pathToFile: String) {
+    val filename = getFileName(pathToFile)?.replace(".csv", ".kt")
+    val file: File
+    if (filename == null) {
+        println("No file")
+        return
+    } else {
+        file = File(filename)
+    }
+    val lines = getCSVLines(pathToFile)
+    val linesMap = lines?.map { "val ${it.split(";")[0]}: ${it.split(";")[2].replace("\n", "")}"}
+    val stringFromLines = linesMap?.joinToString(",")
+    val dataClass = "data class ${filename.replace(".kt", "")}(" +
+            "$stringFromLines\n){}"
+    file.writeText(dataClass)
 }
 
